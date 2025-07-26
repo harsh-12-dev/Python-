5eaa2883bcd5f3f1469c9a49ff3707c5dafcc575
Harsh Verma <artificialintelligenca@gmail.com>
2025-07-26 14:55:33 +0530

    Update day3.py

--- day3.py
+++ day3.py
@@ -1,4 +1,10 @@
 if 99 > 75:
-    print("ninety nine is greater than seventy five")
-    if 56 < 92:
-        print("fifty six is smaller than ninety two")
+ print("ninety nine is greater than seventy five")
+  if 56 < 92:
+   print("fifty six is smaller than ninety two")
+
+# This is indentation errors because python is a case sensitive language.
+if 99 > 75:
+print("ninety nine is greater than seventy five")
+  if 56 < 92:
+  print("fifty six is smaller than ninety two")
